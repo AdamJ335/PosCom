e6318c6f66ee0dfc3252536183034fdb68492481
Adam Jennings <ajennings@tier2consulting.com>
2021-10-14 12:03:02 +0100

    [major] Initialize

---
+++ src/main/java/com/poscom/PosComApplication.java
@@ -0,0 +1,13 @@
+package com.poscom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosComApplication.class, args);
+	}
+
+}
